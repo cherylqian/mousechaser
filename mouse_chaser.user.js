286c5f9143da272df6d95235b2ff1ec27060adf9
Chen Qian <chenq2@illinois.edu>
2018-11-08 19:57:46 -0600

    init

---
+++ mouse_chaser.user.js
@@ -0,0 +1,32 @@
+// ==UserScript==
+// @name         Mouse chaser
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  research tool
+// @author       Chen, Will
+// @match        *://*/*
+// @grant        none
+// @copyright 2018, cherylqian (https://openuserjs.org//users/cherylqian)
+// @license MIT
+// ==/UserScript==
+
+(function () {
+  'use strict';
+  window.onload = () => {
+    let mouseChaser = document.getElementsByTagName("body")[0];
+    let newDiv = mouseChaser.cloneNode(true);
+    newDiv.style.zIndex = -1;
+    newDiv.style.filter = "blur(2rem)";
+    document.body.appendChild(newDiv);
+  };
+  window.onmousemove = (event) => {
+    const width = document.body.clientWidth;
+    const height = document.body.clientHeight;
+    const boxSize = 100;
+    let x = event.clientX;
+    let y = event.clientY;
+    document.getElementsByTagName("body")[0].style.clipPath =
+      `inset(${y - boxSize}px ${width - x - boxSize}px ${height - y - boxSize}px ${x - boxSize}px)`;
+  };
+})();
+
